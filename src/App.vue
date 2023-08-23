e1ae6e9ec93b39d84ab75bc613d6d8c7c875b7e5
Thomas Kientz <thomas@kientz.fr>
2023-08-23 10:51:46 +0200

    repro

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <div class="d-flex">
+        <v-spacer></v-spacer>
+        space left
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts"></script>
